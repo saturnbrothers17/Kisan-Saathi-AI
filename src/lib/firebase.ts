7f02e7bb3f84958e7198286cb0825e69f1544197
jaiprakash mishra <jpm2005physics@gmail.com>
2025-07-06 15:37:03 +0000

    but i cant able to see sign in button in app on that domain

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyBnGzgfBEOd0gqGx68dQZ6WgwCcxpvqPsY",
+  authDomain: "kisan-saathi-ai.firebaseapp.com",
+  projectId: "kisan-saathi-ai",
+  storageBucket: "kisan-saathi-ai.appspot.com",
+  messagingSenderId: "691962974417",
+  appId: "1:691962974417:web:b4f404eef99ca8a45075f0"
 };
 
 // Initialize Firebase
